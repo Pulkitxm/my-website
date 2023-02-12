b9ac73b8c509d4a2582d2f500603afb2500c0d53
Sid <siddhant997@gmail.com>
2023-02-12 15:36:58 -0500

    working code for education card css

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,10 +1,23 @@
 import React from "react";
 import EducationCard from "./EducationCard";
-
+import northeastern from '../../assets/NortheasternLogo.svg'
+import sppu from '../../assets/SPPU_logo.jpeg'
 export default function Education() {
-    return(<>
-    <EducationCard/>
-    <EducationCard/>
-    </>
+    const northesatern_deets = {
+        schoolname: "Northeastern University",
+        schooldeets: "I am studying Computer Science at the Khoury College of Computer Sciences.",
+        timeframe: "September 2021 - May 2023"
+    };
+    const nbn_deets = {
+        schoolname: "Savitribai Phule Pune University",
+        schooldeets:`I spent 4 memorable years at Birla Institute of Technology and Science, a premier engineering college in picturesque Pilani! At BITS, I was the coordinator of the Department of Stage Controls, a body responsible for planning and logistics of Auditorium events during Oasis. I was also a member of ACM, BITS Pilani chapter, and the college football team.
+        A few lines are not enough to show my gratitude, but I came here looking for Copper, and I found Gold.`,
+        timeframe:"August 2016 - May 2020"
+    };
+    const n = 3; //number of education cards
+    return(<div className={"align-text grid grid-rows-"+n}>
+    <EducationCard flip="true"  logo={northeastern} schoolname={northesatern_deets.schoolname} schooldeets={northesatern_deets.schooldeets} timeframe={northesatern_deets.timeframe}/>
+    <EducationCard flip="true" logo={sppu} schoolname={nbn_deets.schoolname} schooldeets={nbn_deets.schooldeets} timeframe={nbn_deets.timeframe}/>
+    </div>
     );
 }
--- src/components/Education/EducationCard.js
+++ src/components/Education/EducationCard.js
@@ -1,17 +1,33 @@
 import React from "react";
 export default function EducationCard(args) {
-    return(
-        <div class="flex justify-center m-2">
-  <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white dark:bg-neutral-300 shadow-lg">
-    <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg" alt="" />
-    <div class="p-6 flex flex-col justify-start">
-      <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+    const text_container = (schoolname, schooldeets,timeframe) => {
+        return(
+            <div class="p-6 flex flex-col justify-start">
+      <h5 class="text-gray-900 text-xl font-medium mb-2">{schoolname}</h5>
       <p class="text-gray-700 text-base mb-4">
-        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+        {schooldeets}
       </p>
-      <p class="text-gray-600 text-xs">Last updated 3 mins ago</p>
+      <p class="text-gray-600 text-xs">{timeframe}</p>
     </div>
-  </div>
+        )
+    }
+    return(
+        <div class="flex justify-center mb-2">
+             <div class="flex flex-col md:flex-row  rounded-lg bg-white dark:bg-neutral shadow-lg">
+                { !args.flip?
+                
+                    <>
+                    <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src={args.logo} alt="" />
+                    {text_container(args.schoolname, args.schooldeets, args.timeframe)}
+                    </>
+                : 
+                    <>
+                        
+                        {text_container(args.schoolname, args.schooldeets, args.timeframe)}
+                        <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-b-lg md:rounded-none md:rounded-r-lg" src={args.logo} alt="" />
+                    </>
+        }
+    </div>
 </div>
     )
 }
