6814a611bf7efde14be4ef87ef88e663292a4eb7
Sid <siddhant997@gmail.com>
2023-02-20 23:39:22 -0500

    ExperienceCard is now functional (almost) need to complete the width constraint.

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,9 +1,20 @@
 import ExperienceCard from './ExperienceCard.js';
+
+
+//Companies
+import IotIot from "../../assets/Experiences/IoTIOT_in.png";
+import Globant from "../../assets/Experiences/Globant.svg";
+
+//Data About the Companies
+import IoTIoT from "./IoTIoTPoints.json";
+import GlobantPoints from "./Globant.json";
+
+
 const Experience=()=> {
     return(
         <div className='flex flex-col my-10 gap-4'>
-            <ExperienceCard/>
-            <ExperienceCard/>
+            <ExperienceCard Logo={IotIot} CompanyName = {IoTIoT['Company Name']} Points = {IoTIoT.Points}/>
+            <ExperienceCard Logo={Globant} CompanyName = {GlobantPoints['Company Name']} Points = {GlobantPoints.Points}/>
         </div>
     )
 }
--- src/components/Experience/ExperienceCard.js
+++ src/components/Experience/ExperienceCard.js
@@ -1,17 +1,20 @@
-import IotIot from "../../assets/Experiences/IoTIOT_in.png"
+
 const ExperienceCard = (args) => {
 return(
 <div class="flex flex-col md:flex-row grow ">
-  <div class="items-center md:w-1/3 border-4 ">
-    <img src={IotIot} className="inline h-44"/>
-  </div>
-  <div class=" md:w-2/3 border-4">
-    <h4 className="md:text-5xl mb-2">IoTIoT.in</h4>
+  
+  <div class=" md:w-3/4">
+    <h4 className="md:text-4xl mb-2 inline">{args.CompanyName}</h4>
 <ul className="text-justify">
-<li>Conducted experiments with Convolutional Neural Networks (CNNs) to create a facial recoginition system
-that is cost efficient and requires as less as 15 data points of the face.</li>
-<li>Used TensorFlow, Keras, NumPy and other libraries based in Python and interfaced it with the startup’s ShunyaOS on Raspberry Pi, reached accuracy levels of about 91%.</li>
+{args.Points.map((value) =>
+{
+    return (<li>{value}</li>)
+}
+)}
 </ul>
+  </div>
+  <div class="items-center md:w-1/4 m-4 ">
+    <img src={args.Logo} className="inline h-40"/>
   </div>
 </div>
 
