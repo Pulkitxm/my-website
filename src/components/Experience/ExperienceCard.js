3fae20b681dc942356bdd1319dd64f5997d455b6
Sid <siddhant997@gmail.com>
2023-02-20 19:28:00 -0500

    ExperienceCard Title included

--- src/components/Experience/ExperienceCard.js
+++ src/components/Experience/ExperienceCard.js
@@ -5,7 +5,14 @@ return(
   <div class="items-center md:w-1/3 border-4 ">
     <img src={IotIot} className="inline h-44"/>
   </div>
-  <div class=" md:w-2/3 border-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+  <div class=" md:w-2/3 border-4">
+    <h4 className="md:text-5xl mb-2">IoTIoT.in</h4>
+<ul className="text-justify">
+<li>Conducted experiments with Convolutional Neural Networks (CNNs) to create a facial recoginition system
+that is cost efficient and requires as less as 15 data points of the face.</li>
+<li>Used TensorFlow, Keras, NumPy and other libraries based in Python and interfaced it with the startup’s ShunyaOS on Raspberry Pi, reached accuracy levels of about 91%.</li>
+</ul>
+  </div>
 </div>
 
 );
