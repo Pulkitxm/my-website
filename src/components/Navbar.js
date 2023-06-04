f12bde734b8fb6407368f548b22a2ec4d9c3f626
Sid <siddhant997@gmail.com>
2023-06-04 10:02:43 -0400

    Adding a hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -183,10 +183,10 @@ return (
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               <button
                 type="button"
-                className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                // className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
               >
-                <span className="sr-only">View notifications</span>
-                <BellIcon className="h-6 w-6" aria-hidden="true" />
+                <span className="sr-only">View Resume</span>
+                <a href={resume} rel="noreferrer" target="_blank" className=" bg-teal-500 rounded-full px-4 py-1 border border-teal-800 border-4">Resume</a>
               </button>
 
               {/* Profile dropdown */}
